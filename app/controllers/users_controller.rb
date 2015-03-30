bb298d1f528449a918bc9b0e411c38ba7384aed6
fabianVeliz <velizfabianhoracio@gmail.com>
2015-03-30 01:32:35 -0300

    Update header and devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   expose(:slug)  { params[:slug] }
   expose(:user)  { User.friendly.find(slug) }
   expose(:tweets){ user.tweets }
