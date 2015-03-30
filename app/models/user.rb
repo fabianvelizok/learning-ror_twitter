8200a75fccfec91da122b848d62649f9f776f8fc
fabianVeliz <velizfabianhoracio@gmail.com>
2015-03-30 00:43:25 -0300

    Add friendly_id gem and add slug field to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  extend FriendlyId
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -7,8 +9,12 @@ class User < ActiveRecord::Base
                         medium: "300x300>",
                         thumb: "40x40#"
                     }
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :user_name, presence: true, uniqueness: { case_sensitive: false }
 
+  friendly_id :user_name, use: :slugged
+
   has_many :tweets
+
 end
