200d3dc6c8d6080b9253bf08305c7e68938b7090
fabianVeliz <velizfabianhoracio@gmail.com>
2015-04-04 02:12:49 -0300

    [Dislike] - User can destroy his likes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   expose(:tweets){ Tweet.where(user_id: current_user.followed_users_and_me).latest }
 
-  def index;  end
+  def index; end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,20 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  expose(:tweet_id){ params[:tweet] }
+  expose(:tweet_id){ params[:id] }
 
   def create
-    like = current_user.likes.build(tweet_id: tweet_id)
-    if like.save
-      redirect_to root_path, notice: 'The like has been created successfully'
+    if current_user.can_give_like?(tweet_id)
+      like = current_user.likes.build(tweet_id: tweet_id)
+      if like.save
+        redirect_to root_path, notice: 'The like has been created successfully'
+      end
     end
   end
 
   def destroy
+    like = current_user.likes.where(tweet_id: tweet_id).first
+    if like.destroy
+      redirect_to root_path, notice: 'The like has been destroyed successfully'
+    end
   end
 end
